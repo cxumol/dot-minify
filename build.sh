25cbcd58d8d51e74de966b1edafe3e47e459e552
cxumol <cxumol@gmail.com>
2024-11-23 01:37:47 +0000

    build: mod build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-# workaround before build.zig in use
+# test
+zig test src/test.zig
+
+# wasm
 zig build-exe src/main.zig -O ReleaseFast -fstrip -target wasm32-wasi
 rm *.wasm.o && mv main.wasm zig-out/bin
 npx wrangler@wasm publish --dry-run --compatibility-date 2024-10-19 --name dot-minify zig-out/bin/main.wasm
-
+# regular
 zig build-exe src/main.zig -O ReleaseSmall -fstrip -fsingle-threaded -target x86_64-linux
 zig build-exe src/main.zig -O ReleaseSafe -fstrip -fsingle-threaded -target x86_64-linux
 zig build -Doptimize=ReleaseSafe --release=small --summary all
